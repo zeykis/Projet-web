599e084ccd504d2963e07ab975d355877ef97fd1
zeykis <thibault1.gomes@epita.fr>
2025-05-19 18:49:08 +0200

    picture now changes to a gif on hover, added navbar using daisyui

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-newSrc = "src/assets/200w.gif";
-// Change image source on hover
 function changeImageOnHover() {
-  const images = document.getElementById("tibo").querySelectorAll("img");
-  images.forEach((image) =>
-    image.addEventListener("mouseover", () => {
-      const newSrc = image.getAttribute("data-hover");
-      if (newSrc) {
-        image.setAttribute("src", newSrc);
-      }
-    })
-  );
+  const img = document.getElementById('tibo');
+  img.src = 'src/assets/200w.gif';
 }
+
+function restoreOriginalImage() {
+  const img = document.getElementById('tibo');
+  img.src = 'src/assets/Shuichi_Saihara_Portrait.webp';
+}
+
+function goToCalculatrice(event) {
+    event.preventDefault();
+    document.getElementById("body_index").style.display = "none";
+    const target = document.getElementById('Calculatrice');
+    target.style.display = 'block';
+    target.scrollIntoView({ behavior: 'smooth' });
+  }

b6d0380d3aa3ef67fc6c27f0ed85b0d3956632c7
zeykis <thibault1.gomes@epita.fr>
2025-05-25 18:48:20 +0200

    Changed the title to match the name change inside the CV

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  root: '.',         
+  base: './',         
+  build: {
+    rollupOptions: {
+    input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        calculatrice: resolve(__dirname, 'calculatrice.html')
+    }
+    },
+    outDir: 'dist',
+        
+    emptyOutDir: true,
+  },
   plugins: [
     tailwindcss()
   ],
